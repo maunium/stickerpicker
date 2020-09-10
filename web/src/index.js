81caaa2ebed066f80939b04f565b307ec0dd2d61
Tulir Asokan <tulir@maunium.net>
2020-09-10 16:12:09 +0300

    Split web things into subdirectories

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import { html, render, Component } from "./lib/htm/preact.js"
+import { html, render, Component } from "../lib/htm/preact.js"
 import { Spinner } from "./spinner.js"
 import * as widgetAPI from "./widget-api.js"
 import * as frequent from "./frequently-used.js"
--- web/src/spinner.js
+++ web/src/spinner.js
@@ -3,7 +3,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import { html } from "https://unpkg.com/htm/preact/index.mjs?module"
+import { html } from "../lib/htm/preact.js"
 
 export const Spinner = ({ size = 40, noCenter = false, noMargin = false, green = false }) => {
 	let margin = 0
