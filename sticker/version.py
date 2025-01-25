0d49cfd3c5febe0215c57b3810db22c1372d3460
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-01-25 23:28:35 +0100

    remove build files from git

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = 'cb68a29a'
+version = '0.1.0+dev.cb68a29a'
+linkified_version = '0.1.0+dev.[cb68a29a](https://github.com/maunium/stickerpicker/commit/cb68a29a7566ffde52b9c875ffe3436dc3c4024d)'
